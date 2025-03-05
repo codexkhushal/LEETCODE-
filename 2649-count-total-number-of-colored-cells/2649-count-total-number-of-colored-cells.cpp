4fb0a423e384fb3dc7d5d1c71011483551ccde4a
codexkhushal <119686147+khushalsharma2213@users.noreply.github.com>
2025-03-05 12:55:27 +0530

    Time: 0 ms (100%), Space: 7.9 MB (61.42%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        int t =1 ;
-        long long cells = 1 ;
-
-        while(t<=n){
-            cells+= (4*(t-1));
-            t++;
-        }
-        return cells;
+       return 1 + (2 * (long long)(n-1)*n);
     }
 
 };
