35af7e398d91cd6ca9c7b9a12708f7ad6e20c4c5
codexkhushal <119686147+khushalsharma2213@users.noreply.github.com>
2025-05-17 01:02:41 +0530

    Time: 5 ms (60.49%), Space: 17.1 MB (70.91%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        int n = s.length();
-        long long swap =  0;
-        int black = 0 ;
-        for(int i = 0 ;i < n ;i ++){
-            if(s[i]=='0') {
-                swap+=black; 
-            }
-            else{
-                black++;
+        long long steps = 0;
+        long long countZero = 0;
+        
+        for (int i = s.size() - 1; i >= 0; --i) {
+            if (s[i] == '0') {
+                countZero++;
+            } else {                
+                steps += countZero;
             }
         }
-        return swap;
+
+        return steps;
     }
 };
