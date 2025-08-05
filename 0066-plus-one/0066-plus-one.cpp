bb389bd63301a0c444f23235c754a0ad0a2321e3
Khushal <119686147+codexkhushal@users.noreply.github.com>
2025-08-05 12:55:58 +0530

    Time: 0 ms (100%), Space: 11.6 MB (34.67%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -13,22 +13,37 @@ public:
         // }
         // return digits;
 
-        int i = digits.size()-1;
-        while (i >= 0) {
-            if (digits[i] < 9) {
+        // int i = digits.size()-1;
+        // while (i >= 0) {
+        //     if (digits[i] < 9) {
+        //         digits[i] += 1;
+        //         return digits;
+        //     }
+        //     else {
+        //         digits[i] = 0;
+        //         i--;
+        //     }
+        // }
+
+        
+        // digits[0] = 1;
+        // digits.push_back(0);
+
+        // return digits;
+
+       
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] + 1 != 10) {
                 digits[i] += 1;
                 return digits;
             }
-            else {
-                digits[i] = 0;
-                i--;
+            digits[i] = 0;
+            if (i == 0) {
+                digits.insert(digits.begin(), 1);
+                return digits;
             }
         }
-
-        
-        digits[0] = 1;
-        digits.push_back(0);
-
-        return digits;
+        return digits;        
+    
     }
 };
