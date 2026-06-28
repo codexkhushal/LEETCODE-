2ec367d1e79644eca473e43f0d42bb30943da16b
Khushal <119686147+codexkhushal@users.noreply.github.com>
2026-06-28 21:46:45 +0530

    Time: 10 ms (29.58%), Space: 77.2 MB (92.08%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -4,15 +4,15 @@ class Solution {
 
         int maxV = -1 ;
         Arrays.sort(arr);
-        for(int i = 0 ; i < n ; i++){
-            if(i==0){
-                arr[0] = 1;
-            }else if(Math.abs(arr[i] - arr[i-1]) > 1){
+        arr[0] = 1;
+
+        for(int i = 1 ; i < n ; i++){
+             if(Math.abs(arr[i] - arr[i-1]) > 1){
                 arr[i] = arr[i-1] + 1 ;
 
             }
-            maxV = Math.max(maxV , arr[i]);
+           
         }
-        return maxV;
+        return arr[n-1];
     }
 }
