5c0cf27979cf7b729371d8c735e130a80983f40c
Khushal <119686147+codexkhushal@users.noreply.github.com>
2026-08-23 15:01:37 +0530

    Time: 8 ms (53.28%), Space: 47.3 MB (10.95%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int sumDiff = 0;
+        int qDiff = 0;
+
+        for (int i = 0; i < n; i++) {
+            char c = num.charAt(i);
+            int sign = (i < n / 2) ? 1 : -1;
+            if (c == '?') {
+                qDiff += sign;
+            } else {
+                sumDiff += sign * (c - '0');
+            }
+        }
+
+        return (qDiff * 9 != -2 * sumDiff);
+    }
+}
